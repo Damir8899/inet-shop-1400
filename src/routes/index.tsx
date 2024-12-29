15613b1887686c421a7536c26b21b5f0d7651eba
Qwerty <abd@abd.ru>
2024-12-29 16:07:38 +0500

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter,createHashRouter } from "react-router";
+import { Paths } from "./paths";
+import Menu from "../pages/Menu";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+export const router = createHashRouter([
+    {
+        path: Paths.menu,
+        element: <Menu/>
+    },
+    {
+        path: Paths.login,
+        element: <Login/>
+    },
+    {
+        path: Paths.register,
+        element: <Register/>
+    },
+])
